91aaf93b261cddbcc7eea3e338a19ccbd685f868
serimikku1209-cyber <serimikku1209@gmail.com>
2026-04-29 13:13:51 +0900

    スコアの初期表示インビジブル設定

--- Assets/Scripts/Gameplay/ScoreManager.cs
+++ Assets/Scripts/Gameplay/ScoreManager.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // GameObjectではなくCanvasGroupをアタッチする
+    [SerializeField] private CanvasGroup scoreTableCanvasGroup;
+
     void Start()
     {
-        
+        // 最初は非表示
+        HideScoreTable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HideScoreTable()
     {
-        
+        if (scoreTableCanvasGroup != null)
+        {
+            scoreTableCanvasGroup.alpha = 0f;          // 透明にする
+            scoreTableCanvasGroup.interactable = false; // ボタンを押せなくする
+            scoreTableCanvasGroup.blocksRaycasts = false; // マウス反応を消す
+        }
+    }
+
+    public void ShowScoreTable()
+    {
+        if (scoreTableCanvasGroup != null)
+        {
+            scoreTableCanvasGroup.alpha = 1f;          // 表示する
+            scoreTableCanvasGroup.interactable = true;  // ボタンを押せるようにする
+            scoreTableCanvasGroup.blocksRaycasts = true; // マウス反応を戻す
+        }
     }
 }
